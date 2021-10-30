86826436bf6a9d2694198355b6a08a29b4137710
AkraichLand <54623031+AkraichLand@users.noreply.github.com>
2021-10-30 03:34:16 +0000

    Adding the full list of leagues

--- leagues.ts
+++ leagues.ts
@@ -39,4 +39,367 @@ export const leagues = [
     id: '11',
     link: 'https://www.soccerstats.com/results.asp?league=england&pmtype=bydate',
   },
+  {
+    id: '12',
+    link: 'https://www.soccerstats.com/results.asp?league=argentina&pmtype=bydate',
+  },
+
+  {
+    id: '13',
+    link: 'https://www.soccerstats.com/results.asp?league=armenia&pmtype=bydate',
+  },
+
+  {
+    id: '14',
+    link: 'https://www.soccerstats.com/results.asp?league=austria&pmtype=bydate',
+  },
+
+  {
+    id: '15',
+    link: 'https://www.soccerstats.com/results.asp?league=austria2&pmtype=bydate',
+  },
+
+  {
+    id: '16',
+    link: 'https://www.soccerstats.com/results.asp?league=belgium&pmtype=bydate',
+  },
+
+  {
+    id: '17',
+    link: 'https://www.soccerstats.com/results.asp?league=belgium2&pmtype=bydate',
+  },
+
+  {
+    id: '18',
+    link: 'https://www.soccerstats.com/results.asp?league=belarus&pmtype=bydate',
+  },
+
+  {
+    id: '19',
+    link: 'https://www.soccerstats.com/results.asp?league=bolivia&pmtype=bydate',
+  },
+
+  {
+    id: '20',
+    link: 'https://www.soccerstats.com/results.asp?league=brazil&pmtype=bydate',
+  },
+
+  {
+    id: '21',
+    link: 'https://www.soccerstats.com/results.asp?league=brazil2&pmtype=bydate',
+  },
+
+  {
+    id: '22',
+    link: 'https://www.soccerstats.com/results.asp?league=brazil3&pmtype=bydate',
+  },
+
+  {
+    id: '23',
+    link: 'https://www.soccerstats.com/results.asp?league=cyprus&pmtype=bydate',
+  },
+  {
+    id: '24',
+    link: 'https://www.soccerstats.com/results.asp?league=costarica&pmtype=bydate',
+  },
+  {
+    id: '25',
+    link: 'https://www.soccerstats.com/results.asp?league=croatia&pmtype=bydate',
+  },
+  {
+    id: '26',
+    link: 'https://www.soccerstats.com/results.asp?league=denmark&pmtype=bydate',
+  },
+  {
+    id: '27',
+    link: 'https://www.soccerstats.com/results.asp?league=denmark2&pmtype=bydate',
+  },
+  {
+    id: '29',
+    link: 'https://www.soccerstats.com/results.asp?league=uae&pmtype=bydate',
+  },
+  {
+    id: '30',
+    link: 'https://www.soccerstats.com/results.asp?league=ecuador2&pmtype=bydate',
+  },
+  {
+    id: '31',
+    link: 'https://www.soccerstats.com/results.asp?league=spain&pmtype=bydate',
+  },
+  {
+    id: '32',
+    link: 'https://www.soccerstats.com/results.asp?league=spain2&pmtype=bydate',
+  },
+  {
+    id: '33',
+    link: 'https://www.soccerstats.com/results.asp?league=estonia&pmtype=bydate',
+  },
+  {
+    id: '34',
+    link: 'https://www.soccerstats.com/results.asp?league=finland&pmtype=bydate',
+  },
+  {
+    id: '35',
+    link: 'https://www.soccerstats.com/results.asp?league=finland2&pmtype=bydate',
+  },
+  {
+    id: '36',
+    link: 'https://www.soccerstats.com/results.asp?league=france&pmtype=bydate',
+  },
+  {
+    id: '37',
+    link: 'https://www.soccerstats.com/results.asp?league=france2&pmtype=bydate',
+  },
+  {
+    id: '39',
+    link: 'https://www.soccerstats.com/results.asp?league=france3&pmtype=bydate',
+  },
+  {
+    id: '40',
+    link: 'https://www.soccerstats.com/results.asp?league=netherlands&pmtype=bydate',
+  },
+  {
+    id: '41',
+    link: 'https://www.soccerstats.com/results.asp?league=netherlands2&pmtype=bydate',
+  },
+  {
+    id: '42',
+    link: 'https://www.soccerstats.com/results.asp?league=hungary&pmtype=bydate',
+  },
+  {
+    id: '43',
+    link: 'https://www.soccerstats.com/results.asp?league=hungary2&pmtype=bydate',
+  },
+  {
+    id: '44',
+    link: 'https://www.soccerstats.com/results.asp?league=ireland&pmtype=bydate',
+  },
+  {
+    id: '45',
+    link: 'https://www.soccerstats.com/results.asp?league=ireland2&pmtype=bydate',
+  },
+  {
+    id: '47',
+    link: 'https://www.soccerstats.com/results.asp?league=italy&pmtype=bydate',
+  },
+  {
+    id: '48',
+    link: 'https://www.soccerstats.com/results.asp?league=italy2&pmtype=bydate',
+  },
+  {
+    id: '49',
+    link: 'https://www.soccerstats.com/results.asp?league=japan&pmtype=bydate',
+  },
+  {
+    id: '50',
+    link: 'https://www.soccerstats.com/results.asp?league=japan2&pmtype=bydate',
+  },
+  {
+    id: '54',
+    link: 'https://www.soccerstats.com/results.asp?league=saudiarabia&pmtype=bydate',
+  },
+  {
+    id: '55',
+    link: 'https://www.soccerstats.com/results.asp?league=mexico&pmtype=bydate',
+  },
+  {
+    id: '57',
+    link: 'https://www.soccerstats.com/results.asp?league=montenegro&pmtype=bydate',
+  },
+  {
+    id: '58',
+    link: 'https://www.soccerstats.com/results.asp?league=northernireland&pmtype=bydate',
+  },
+  {
+    id: '59',
+    link: 'https://www.soccerstats.com/results.asp?league=norway&pmtype=bydate',
+  },
+  {
+    id: '60',
+    link: 'https://www.soccerstats.com/results.asp?league=norway2&pmtype=bydate',
+  },
+  {
+    id: '61',
+    link: 'https://www.soccerstats.com/results.asp?league=peru2&pmtype=bydate',
+  },
+  {
+    id: '62',
+    link: 'https://www.soccerstats.com/results.asp?league=poland&pmtype=bydate',
+  },
+  {
+    id: '63',
+    link: 'https://www.soccerstats.com/results.asp?league=poland2&pmtype=bydate',
+  },
+  {
+    id: '64',
+    link: 'https://www.soccerstats.com/results.asp?league=poland3&pmtype=bydate',
+  },
+  {
+    id: '65',
+    link: 'https://www.soccerstats.com/results.asp?league=portugal&pmtype=bydate',
+  },
+  {
+    id: '66',
+    link: 'https://www.soccerstats.com/results.asp?league=portugal2&pmtype=bydate',
+  },
+  {
+    id: '69',
+    link: 'https://www.soccerstats.com/results.asp?league=russia&pmtype=bydate',
+  },
+  {
+    id: '70',
+    link: 'https://www.soccerstats.com/results.asp?league=russia2&pmtype=bydate',
+  },
+  {
+    id: '71',
+    link: 'https://www.soccerstats.com/results.asp?league=scotland&pmtype=bydate',
+  },
+  {
+    id: '72',
+    link: 'https://www.soccerstats.com/results.asp?league=scotland2&pmtype=bydate',
+  },
+  {
+    id: '73',
+    link: 'https://www.soccerstats.com/results.asp?league=scotland3&pmtype=bydate',
+  },
+  {
+    id: '74',
+    link: 'https://www.soccerstats.com/results.asp?league=scotland4&pmtype=bydate',
+  },
+  {
+    id: '75',
+    link: 'https://www.soccerstats.com/results.asp?league=serbia&pmtype=bydate',
+  },
+  {
+    id: '76',
+    link: 'https://www.soccerstats.com/results.asp?league=sweden&pmtype=bydate',
+  },
+  {
+    id: '77',
+    link: 'https://www.soccerstats.com/results.asp?league=sweden2&pmtype=bydate',
+  },
+  {
+    id: '78',
+    link: 'https://www.soccerstats.com/results.asp?league=sweden3&pmtype=bydate',
+  },
+  {
+    id: '79',
+    link: 'https://www.soccerstats.com/results.asp?league=sweden4&pmtype=bydate',
+  },
+  {
+    id: '80',
+    link: 'https://www.soccerstats.com/results.asp?league=switzerland&pmtype=bydate',
+  },
+  {
+    id: '81',
+    link: 'https://www.soccerstats.com/results.asp?league=switzerland2&pmtype=bydate',
+  },
+  {
+    id: '82',
+    link: 'https://www.soccerstats.com/results.asp?league=slovakia&pmtype=bydate',
+  },
+  {
+    id: '83',
+    link: 'https://www.soccerstats.com/results.asp?league=slovenia&pmtype=bydate',
+  },
+  {
+    id: '84',
+    link: 'https://www.soccerstats.com/results.asp?league=czechrepublic&pmtype=bydate',
+  },
+  {
+    id: '85',
+    link: 'https://www.soccerstats.com/results.asp?league=czechrepublic2&pmtype=bydate',
+  },
+  {
+    id: '86',
+    link: 'https://www.soccerstats.com/results.asp?league=turkey&pmtype=bydate',
+  },
+  {
+    id: '87',
+    link: 'https://www.soccerstats.com/results.asp?league=turkey2&pmtype=bydate',
+  },
+  {
+    id: '88',
+    link: 'https://www.soccerstats.com/results.asp?league=ukraine&pmtype=bydate',
+  },
+  {
+    id: '89',
+    link: 'https://www.soccerstats.com/results.asp?league=usa&pmtype=bydate',
+  },
+  {
+    id: '90',
+    link: 'https://www.soccerstats.com/results.asp?league=usa2&pmtype=bydate',
+  },
+  {
+    id: '92',
+    link: 'https://www.soccerstats.com/results.asp?league=algeria&pmtype=bydate',
+  },
+  {
+    id: '95',
+    link: 'https://www.soccerstats.com/results.asp?league=argentina2&pmtype=bydate',
+  },
+  {
+    id: '96',
+    link: 'https://www.soccerstats.com/results.asp?league=argentina9&pmtype=bydate',
+  },
+  {
+    id: '99',
+    link: 'https://www.soccerstats.com/results.asp?league=morocco2&pmtype=bydate',
+  },
+  {
+    id: '100',
+    link: 'https://www.soccerstats.com/results.asp?league=morocco&pmtype=bydate',
+  },
+  {
+    id: '101',
+    link: 'https://www.soccerstats.com/results.asp?league=italy3&pmtype=bydate',
+  },
+  {
+    id: '102',
+    link: 'https://www.soccerstats.com/results.asp?league=italy4&pmtype=bydate',
+  },
+  {
+    id: '103',
+    link: 'https://www.soccerstats.com/results.asp?league=italy5&pmtype=bydate',
+  },
+  {
+    id: '104',
+    link: 'https://www.soccerstats.com/results.asp?league=bosnia&pmtype=bydate',
+  },
+  {
+    id: '105',
+    link: 'https://www.soccerstats.com/results.asp?league=bulgaria&pmtype=bydate',
+  },
+  {
+    id: '106',
+    link: 'https://www.soccerstats.com/results.asp?league=greece&pmtype=bydate',
+  },
+  {
+    id: '108',
+    link: 'https://www.soccerstats.com/results.asp?league=norway3&pmtype=bydate',
+  },
+  {
+    id: '109',
+    link: 'https://www.soccerstats.com/results.asp?league=norway4&pmtype=bydate',
+  },
+  {
+    id: '110',
+    link: 'https://www.soccerstats.com/results.asp?league=chile&pmtype=bydate',
+  },
+  {
+    id: '111',
+    link: 'https://www.soccerstats.com/results.asp?league=colombia2&pmtype=bydate',
+  },
+  {
+    id: '112',
+    link: 'https://www.soccerstats.com/results.asp?league=paraguay2&pmtype=bydate',
+  },
+  {
+    id: '113',
+    link: 'https://www.soccerstats.com/results.asp?league=uruguay2&pmtype=bydate',
+  },
+  {
+    id: '114',
+    link: 'https://www.soccerstats.com/results.asp?league=venezuela&pmtype=bydate',
+  },
 ];
