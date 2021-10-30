c2709cea1604d1a7328dbe5af34f1a7f00e88599
AkraichLand <54623031+AkraichLand@users.noreply.github.com>
2021-10-30 02:25:45 +0000

    Refactoring some code and adding the logFixture function

--- index.ts
+++ index.ts
@@ -2,16 +2,67 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 const logMatch = (match: any) => {
+  const matchDate = match.children[0].children[0].children[0].data.trim();
   const firstTeam = match.children[1].children[0].data.trim();
-  const fullTime = match.children[2].children[0].children[0].children[0].data;
+  const FT = match.children[2].children[0].children[0].children[0].data;
   const secondTeam = match.children[3].children[0].data.trim();
-  const halfTime = match.children[5].children[0].children[0].data.replace(
+  const FHT = match.children[5].children[0].children[0].data.replace(
     /[{()}]/g,
     ''
   );
+  //Fulltime result
+  const HFTG = FT.split(' - ')[0];
+  const AFTG = FT.split(' - ')[1];
+
+  //First halftime result
+  const HFHTG = FHT.split('-')[0];
+  const AFHTG = FHT.split('-')[1];
+
+  //Second halftime result
+  const HSHTG = parseInt(HFTG) - parseInt(HFHTG);
+  const ASHTG = parseInt(AFTG) - parseInt(AFHTG);
 
   console.log(
-    firstTeam + ' | ' + fullTime + ' | ' + secondTeam + ' | ' + halfTime
+    '2' +
+      ' | ' +
+      matchDate +
+      ' | ' +
+      ' ' +
+      ' | ' +
+      firstTeam +
+      ' | ' +
+      secondTeam +
+      ' | ' +
+      HFTG +
+      ' | ' +
+      AFTG +
+      ' | ' +
+      HFHTG +
+      ' | ' +
+      AFHTG +
+      ' | ' +
+      HSHTG +
+      ' | ' +
+      ASHTG
+  );
+};
+
+const logFixture = (match: any) => {
+  const fixtureDate = match.children[0].children[0].children[0].data.trim();
+  const firstTeam = match.children[1].children[0].data.trim();
+  const fixtureTime = match.children[2].children[0].children[0].data;
+  const secondTeam = match.children[3].children[0].data.trim();
+
+  console.log(
+    '0' +
+      ' | ' +
+      fixtureDate +
+      ' | ' +
+      fixtureTime +
+      ' | ' +
+      firstTeam +
+      ' | ' +
+      secondTeam
   );
 };
 
@@ -31,6 +82,8 @@ axios
       let match: any = matches[m];
       if (match.children.length === 9) {
         logMatch(match);
+      } else if (match.children.length === 7) {
+        logFixture(match);
       }
     }
   })
